5ca49e10c6412f3ce03958bd8852927aeba9de42
unpseudocomplique <69119668+unpseudocomplique@users.noreply.github.com>
2020-12-14 13:49:07 +0100

    Add parameters for offset, and limit to product (update doc too)

--- routes/product.js
+++ routes/product.js
@@ -3,10 +3,20 @@ const router = express.Router();
 const Models = require("../Model");
 
 router.get("/", async (req, res) => {
-  const offset = req.query.offset || 0 ;
-  const perPage = req.query.perPage || 2 ;
-  const filter = {};
-  res.json(await Models.Product.find(filter)).limit(perPage).skip(offset*perPage);
+  const offset = parseInt(req.query.offset) || 0;
+  const perPage = parseInt(req.query.perPage) || 2;
+  const name = req.query.name || "";
+  const id = req.query.id || "";
+
+  const filter = { libelle: { $regex: name } };
+
+  if (id) filter._id = id;
+
+  res.json(
+    await Models.Product.find(filter)
+      .limit(perPage)
+      .skip(offset * perPage)
+  );
 });
 
 module.exports = router;
