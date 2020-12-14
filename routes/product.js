56252f23d800b900476c93bdd7ed3cbd52e34482
gptoto <hamonaxe@live.fr>
2020-12-14 13:04:12 +0100

    PerPage

--- routes/product.js
+++ routes/product.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const Models = require("../Model");
 
 router.get("/", async (req, res) => {
+  const offset = req.query.offset || 0 ;
+  const perPage = req.query.perPage || 2 ;
   const filter = {};
-  res.json(await Models.Product.find(filter));
+  res.json(await Models.Product.find(filter)).limit(perPage).skip(offset*perPage);
 });
 
 module.exports = router;
